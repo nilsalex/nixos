f7048b71af1746829be5392e7f93b5fe7c5291ab
Nils Alex <nils.alex@tngtech.com>
2023-10-11 09:35:59 +0200

    Switch to release branch from nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
