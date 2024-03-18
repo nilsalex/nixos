0e76e153377c8266b9e3dafec0ab14505bb9bc8b
Nils Alex <nils@famalex.de>
2024-03-18 19:46:15 +0100

    add native messaging host manifest for gopass to firefox

--- home.nix
+++ home.nix
@@ -509,15 +509,26 @@ exit $?
   "path": "/home/nils/.config/gopass/gopass_wrapper.sh",
   "type": "stdio",
   "allowed_origins": [
-      "chrome-extension://kkhfnlkhiapbiehimabddjbimfaijdhk/"
+    "chrome-extension://kkhfnlkhiapbiehimabddjbimfaijdhk/"
   ]
 }
   '';
 
   programs.firefox = {
     enable = true;
-    nativeMessagingHosts = [ pkgs.gopass-jsonapi ];
   };
+
+  home.file.".mozilla/native-messaging-hosts/com.justwatch.gopass.json".text = ''
+{
+  "name": "com.justwatch.gopass",
+  "description": "Gopass wrapper to search and return passwords",
+  "path": "/home/nils/.config/gopass/gopass_wrapper.sh",
+  "type": "stdio",
+  "allowed_extensions": [
+    "{eec37db0-22ad-4bf1-9068-5ae08df8c7e9}"
+  ]
+}
+  '';
 
   programs.vscode = {
     enable = true;
