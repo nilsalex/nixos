e9a61e020e12109d2780be41bf1152e19f94ae3f
Nils Alex <nils@famalex.de>
2024-08-21 20:11:22 +0200

    flake update, remove overlay

--- overlays.nix
+++ overlays.nix
@@ -10,16 +10,5 @@
         };
       });
     })
-    (final: prev: {
-      awscli2 = prev.awscli2.overrideAttrs (old: {
-        patches = [
-          # Temporary test fix until https://github.com/aws/aws-cli/pull/8838 is merged upstream
-          (pkgs.fetchpatch {
-            url = "https://github.com/aws/aws-cli/commit/b5f19fe136ab0752cd5fcab21ff0ab59bddbea99.patch";
-            hash = "sha256-NM+nVlpxGAHVimrlV0m30d4rkFVb11tiH8Y6//2QhMI=";
-          })
-        ];
-      });
-    })
   ];
 }
