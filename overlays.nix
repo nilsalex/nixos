71414d7f1219d40f2361b066c74284cbcdb35532
Nils Alex <nils@famalex.de>
2024-08-26 17:53:16 +0200

    flake update

--- overlays.nix
+++ overlays.nix
@@ -1,14 +1,6 @@
 { pkgs, ... }:
 {
   nixpkgs.overlays = [
-    (final: prev: {
-      clamav = prev.clamav.overrideAttrs (old: rec {
-        version = "1.4.0";
-        src = pkgs.fetchurl {
-          url = "https://www.clamav.net/downloads/production/${old.pname}-${version}.tar.gz";
-          hash = "sha256-1nqymeXKBdrT2imaXqc9YCCTcqW+zX8TuaM8KQM4pOY=";
-        };
-      });
-    })
+    # (final: prev: { })
   ];
 }
