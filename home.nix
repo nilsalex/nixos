805aec85f7472444735dcd9c5d600afb36e388bc
Nils Alex <nils@famalex.de>
2026-01-12 07:36:58 +0100

    restore old ssh defaults

--- home.nix
+++ home.nix
@@ -577,6 +577,20 @@ in
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
+    matchBlocks = {
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
+    };
   };
 
   home.file.".config/gopass/gopass_wrapper.sh" = {
