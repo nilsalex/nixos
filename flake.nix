1867a4d54e3ef05d83b56688aa3060b81161372f
Nils Alex <nils@famalex.de>
2026-04-24 13:05:16 +0200

    update nvim config

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    neovim-config.url = "github:nilsalex/kickstart.nvim/fork-v2";
+    neovim-config.url = "github:nilsalex/kickstart.nvim/fork-v2-rebased";
   };
 
   outputs =
