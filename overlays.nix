85d70bdb66d46716b5e426712f97d8ad28c75ff6
Nils Alex <nils@famalex.de>
2026-07-07 23:51:53 +0200

    fix xdg-desktop-portal-wlr bug

--- overlays.nix
+++ overlays.nix
@@ -8,5 +8,24 @@
         crit = crit.packages.${final.stdenv.hostPlatform.system}.default;
       }
     )
+    (
+      final: prev:
+      {
+        xdg-desktop-portal-wlr = prev.xdg-desktop-portal-wlr.overrideAttrs (old: {
+          patches = (old.patches or [ ]) ++ [
+            (builtins.toFile "bump-buffers.patch" ''
+              --- a/include/pipewire_screencast.h
+              +++ b/include/pipewire_screencast.h
+              @@ -6,3 +6,3 @@
+              -#define XDPW_PWR_BUFFERS 2
+              -#define XDPW_PWR_BUFFERS_MIN 2
+              +#define XDPW_PWR_BUFFERS 4
+              +#define XDPW_PWR_BUFFERS_MIN 4
+               #define XDPW_PWR_ALIGN 16
+            '')
+          ];
+        });
+      }
+    )
   ];
 }
