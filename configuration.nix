dc77b9a4a9d16566e4455e09a5c7053efe8931b9
Nils Alex <nils@famalex.de>
2026-09-04 16:01:11 +0200

    replace docker with podman

--- configuration.nix
+++ configuration.nix
@@ -63,10 +63,6 @@
       "wheel"
       "video"
       "networkmanager"
-      "libvirtd"
-      "kvm"
-      "qemu-libvirtd"
-      "docker"
     ];
   };
 
@@ -184,7 +180,11 @@
 
   virtualisation.spiceUSBRedirection.enable = true;
 
-  virtualisation.docker.enable = true;
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
